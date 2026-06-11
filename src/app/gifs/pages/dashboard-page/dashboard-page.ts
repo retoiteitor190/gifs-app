a81527ae1914824484f108f37730536365b86fb1
retoiteitor190 <retoiteitor190@hotmail.com>
2026-06-10 22:27:55 -0600

    Tarea_1

--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-page.html',
 })
 export  default class DashboardPage {}
